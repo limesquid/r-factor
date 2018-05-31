b24bf12d6c7f396d8c02c56e3d68b19d241eafbc
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2018-05-31 03:34:55 +0200

    Refactoring tests

--- src/__tests__/generate-prop-types.test.js
+++ src/__tests__/generate-prop-types.test.js
@@ -1,52 +1,34 @@
 const { readFile } = require('./test-utils');
 const GeneratePropTypes = require('../refactorings/generate-prop-types');
 
-describe('generate-prop-types', () => {
-  const refactoring = new GeneratePropTypes();
+const files = [ 1, 2, 3, 4, 5, 6, 7, 8 ].map((n) => `button${n}`).concat(
+  [ 1, 2 ].map((n) => `filter${n}`)
+);
 
-  it('applies refactoring correctly', () => {
-    const tests = [
-      {
-        input: readFile('generate-prop-types/input/button1.js'),
-        output: readFile('generate-prop-types/output/button1.js')
-      },
-      {
-        input: readFile('generate-prop-types/input/button2.js'),
-        output: readFile('generate-prop-types/output/button2.js')
-      },
-      {
-        input: readFile('generate-prop-types/input/button3.js'),
-        output: readFile('generate-prop-types/output/button3.js')
-      },
-      {
-        input: readFile('generate-prop-types/input/button4.js'),
-        output: readFile('generate-prop-types/output/button4.js')
-      },
-      {
-        input: readFile('generate-prop-types/input/button5.js'),
-        output: readFile('generate-prop-types/output/button5.js')
-      },
-      {
-        input: readFile('generate-prop-types/input/button6.js'),
-        output: readFile('generate-prop-types/output/button6.js')
-      },
-      {
-        input: readFile('generate-prop-types/input/button7.js'),
-        output: readFile('generate-prop-types/output/button7.js')
-      },
-      {
-        input: readFile('generate-prop-types/input/button8.js'),
-        output: readFile('generate-prop-types/output/button8.js')
-      },
-      {
-        input: readFile('generate-prop-types/input/filter1.js'),
-        output: readFile('generate-prop-types/output/filter1.js')
-      },
-      {
-        input: readFile('generate-prop-types/input/filter2.js'),
-        output: readFile('generate-prop-types/output/filter2.js')
-      }
-    ];
-    tests.forEach(({ input, output }) => expect(refactoring.refactor(input)).toBe(output));
+describe('generate-prop-types:canApply', () => {
+  const refactoring = new GeneratePropTypes();
+  const tests = files.map((file) => ({
+    name: `generate-prop-types/input/${file}.js`,
+    input: readFile(`generate-prop-types/input/${file}.js`),
+    output: true
+  }));
+  tests.forEach(({ name, input, output }) => {
+    it(`canApply "${name}"`, () => {
+      expect(refactoring.canApply(input)).toBe(output);
+    });
+  });
+});
+
+describe('generate-prop-types:refactor', () => {
+  const refactoring = new GeneratePropTypes();
+  const tests = files.map((file) => ({
+    name: `generate-prop-types/input/${file}.js -> generate-prop-types/output/${file}.js`,
+    input: readFile(`generate-prop-types/input/${file}.js`),
+    output: readFile(`generate-prop-types/output/${file}.js`)
+  }));
+  tests.forEach(({ name, input, output }) => {
+    it(`refactor "${name}"`, () => {
+      expect(refactoring.refactor(input)).toBe(output);
+    });
   });
 });
