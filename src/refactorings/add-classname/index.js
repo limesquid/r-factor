959c03ee3b064011ea4af6f22efcaf723f10b94d
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2018-06-04 01:39:34 +0200

    Using add-prop-types & add-import-declaration

--- src/refactorings/add-classname/index.js
+++ src/refactorings/add-classname/index.js
@@ -2,13 +2,21 @@ const babylon = require('@babel/parser');
 const traverse = require('@babel/traverse').default;
 const { babylonOptions } = require('../../options');
 const { Refactoring } = require('../../model');
+const { addImportDeclaration, addPropTypes } = require('../../transformations');
 const ComponentBuilder = require('./component-builder');
 
 class AddClassname extends Refactoring {
   constructor() {
     super();
     this.transformations = [
-      (code, ast) => this.refactorCode(code, ast)
+      (code, ast) => this.refactorCode(code, ast),
+      (code, ast) => addImportDeclaration(code, ast, {
+        module: 'prop-types',
+        identifier: 'PropTypes'
+      }),
+      (code, ast) => addPropTypes(code, ast, {
+        className: 'PropTypes.string'
+      })
     ];
   }
 
