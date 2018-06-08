da85d7267c05eb0c7f1200f967e6cb0f830b5160
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2018-06-08 03:44:54 +0200

    100% coverage

--- src/__tests__/data/sort-imports/input/alphabetic1.js
+++ src/__tests__/data/sort-imports/input/alphabetic1.js
@@ -1,4 +1,6 @@
 import { push } from 'react-router-redux';
 import { connect } from 'react-redux';
 import styles from './styles.scss';
+import y from '../y';
+import x from '../../x';
 import classNames from 'classnames';
--- src/__tests__/data/sort-imports/output/alphabetic1.js
+++ src/__tests__/data/sort-imports/output/alphabetic1.js
@@ -1,4 +1,6 @@
 import classNames from 'classnames';
 import { connect } from 'react-redux';
 import { push } from 'react-router-redux';
+import x from '../../x';
+import y from '../y';
 import styles from './styles.scss';
