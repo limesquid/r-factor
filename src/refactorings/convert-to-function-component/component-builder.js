bf14a5d6c82777461b12493d041a3dbb41424087
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2018-06-25 04:48:45 +0200

    Eslint, coverage

--- src/refactorings/convert-to-function-component/component-builder.js
+++ src/refactorings/convert-to-function-component/component-builder.js
@@ -1,12 +1,7 @@
 const { Builder } = require('../../model');
 const settings = require('../../settings');
 const { cleanUpCode, indentCode, squeezeCode } = require('../../utils');
-const {
-  getClassMethod,
-  getReturnStatement,
-  isExportDefaultArrowComponentDeclaration,
-  isPropsDeclaration
-} = require('../../utils/ast');
+const { isExportDefaultArrowComponentDeclaration } = require('../../utils/ast');
 
 class ComponentBuilder extends Builder {
   build() {
@@ -57,21 +52,6 @@ class ComponentBuilder extends Builder {
     return declaration;
   }
 
-  buildBodyNonReturnStatements() {
-    const render = getClassMethod(this.node, 'render');
-    const bodyNodes = render.body.body;
-    const firstNode = bodyNodes[0];
-    const lastNonReturnNode = [ ...bodyNodes ].reverse().find((node) => node.type !== 'ReturnStatement');
-    let code = this.code.substring(firstNode.start, lastNonReturnNode.end);
-    code = indentCode(code, -settings.indent);
-    return code;
-  }
-
-  buildJsx() {
-    const jsxNode = this.getReturnStatementValue();
-    return this.code.substring(jsxNode.start, jsxNode.end);
-  }
-
   buildName() {
     if (this.getDeclaration()) {
       return this.getDeclaration().id.name;
@@ -88,19 +68,6 @@ class ComponentBuilder extends Builder {
     return '';
   }
 
-  ensureProperExport(code) {
-    const declarationCode = `const ${this.buildName()}`;
-    const defaultExportCode = `export default ${declarationCode}`;
-    let newCode = code;
-    if (newCode.includes(defaultExportCode)) {
-      newCode = newCode.replace(defaultExportCode, declarationCode);
-      newCode += settings.endOfLine;
-      newCode += `export default ${this.buildName()}${settings.semicolon}`;
-      newCode += settings.endOfLine;
-    }
-    return newCode;
-  }
-
   getDeclaration() {
     if (this.node.declarations) {
       return this.node.declarations[0];
@@ -114,34 +81,6 @@ class ComponentBuilder extends Builder {
       return declaration.init;
     }
     return this.node.declaration;
-  }
-
-  getOldPropsDeclaration() {
-    const propsNode = this.getPropsNode();
-    return this.code.substring(propsNode.start, propsNode.end);
-  }
-
-  getPropsDeclaration() {
-    const propsNode = this.getPropsNode();
-    return propsNode.declarations.find(isPropsDeclaration);
-  }
-
-  getPropsNode() {
-    const render = getClassMethod(this.node, 'render');
-    const bodyNodes = render.body.body;
-    return bodyNodes.find(
-      ({ type, declarations }) => type === 'VariableDeclaration' && declarations.find(isPropsDeclaration)
-    );
-  }
-
-  getReturnStatementValue() {
-    const render = getClassMethod(this.node, 'render');
-    const returnStatement = getReturnStatement(render);
-    return returnStatement.argument;
-  }
-
-  hasPropsDeclaration() {
-    return Boolean(this.getPropsNode() && this.getPropsDeclaration());
   }
 
   returnsJsxImmediately() {
