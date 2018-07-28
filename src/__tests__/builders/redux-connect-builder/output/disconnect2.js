e4bfa05e3c031519614370965b0366e0f67e4052
ziqueq <ziqueq@gmail.com>
2018-07-28 15:19:33 +0200

    Disconnect tests updated

--- src/__tests__/builders/redux-connect-builder/output/disconnect2.js
+++ src/__tests__/builders/redux-connect-builder/output/disconnect2.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const ButtonComponent = () => (<span>123</span>);
+const ButtonComponent = ({ name }) => (<span>{name}</span>);
 
 export const Button = ButtonComponent;
