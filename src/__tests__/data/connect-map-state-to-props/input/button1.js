733bebd246af39dc2329bbc5559d7c186a47cc34
ziqueq <ziqueq@gmail.com>
2018-07-28 15:11:50 +0200

    Tests updated

--- src/__tests__/data/connect-map-state-to-props/input/button1.js
+++ src/__tests__/data/connect-map-state-to-props/input/button1.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const Button = (props) => (
-  <div>Button</div>
+const Button = ({ value }) => (
+  <div>{value}</div>
 );
 
 export default Button;
--- src/__tests__/data/connect-map-state-to-props/output/button1.js
+++ src/__tests__/data/connect-map-state-to-props/output/button1.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const Button = (props) => (
-  <div>Button</div>
+const Button = ({ value }) => (
+  <div>{value}</div>
 );
 
 const mapStateToProps = (state) => ({
