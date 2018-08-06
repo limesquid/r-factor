2649e803112b786c8d799ceb7855e709f2ef4a33
Yuriy Yakym <ziqueq@gmail.com>
2018-08-06 14:02:47 +0200

    License

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const getStdin = require('get-stdin');
 const argv = require('./cli');
 const settings = require('./src/settings');
 const refactorings = require('./src/refactorings');
+const { verifyLicense } = require('./src/utils/license');
 
 settings.set(JSON.parse(argv.settings));
 const refactoring = new refactorings[argv.refactoring]();
 
 getStdin().then((data) => {
   try {
+    if (!verifyLicense(argv.license)) {
+      throw 'Invalid license';
+    }
     if (refactoring.canApply(data)) {
       const code = refactoring.refactor(data);
       process.stdout.write(code);
---
+++ src/utils/license.js
@@ -0,0 +1,21 @@
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+
+// eslint-disable-next-line
+const LICENSE_SECRET = 'YC3@6jyLqq%!b#Br22iL7h9hmn%TYEGw5SoJVCrXWcAAzykoqXHkqX8AwJwbIVlR^PCnzbq%7W5x^&BYZObsr*IbrP&VmxanuxOU';
+
+const md5 = (string) => crypto.createHash('md5').update(string).digest('hex');
+
+const verifyLicense = (license) => {
+  try {
+    const details = jwt.verify(license, LICENSE_SECRET);
+    const { fullname, email, key } = details;
+    return key === md5(`${fullname}:${process.env.LICENSE_SECRET}:${email}`);
+  } catch (error) {
+    return false;
+  }
+};
+
+module.exports = {
+  verifyLicense
+};
