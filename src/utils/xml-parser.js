a84d98be92f29b2b0d97e2ad454f04dbaacbe9cd
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2018-08-14 19:43:41 +0200

    Error handling

--- src/utils/xml-parser.js
+++ src/utils/xml-parser.js
@@ -1,10 +1,17 @@
 const xml2js = require('xml2js');
 
 const parse = (xml) => {
-  let result = '';
+  let result = null;
+  let parsingError = null;
   xml2js.parseString(xml, (error, parsed) => {
+    parsingError = error;
     result = parsed;
   });
+
+  if (parsingError) {
+    throw parsingError;
+  }
+
   return result;
 };
 
