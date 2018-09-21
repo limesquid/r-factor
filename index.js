de791f75f227421f20d9497b4c2d48367fb8c53e
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2018-09-21 22:43:44 +0200

    Adding warmup

--- index.js
+++ index.js
@@ -2,6 +2,26 @@ const globalSettings = require('./src/settings');
 const refactorings = require('./src/refactorings');
 const verifyLicense = require('./src/license/verify');
 
+const WARMUP_CODE = `
+import React from 'react';
+
+const Component = ({ children, onClick }) => (
+  <div onClick={onClick}>
+    {children}
+  </div>
+);
+
+export default Component;
+`;
+
+const warmup = () => {
+  const GeneratePropTypes = refactorings['generate-prop-types'];
+  const generatePropTypes = new GeneratePropTypes();
+  return generatePropTypes.refactor(WARMUP_CODE);
+};
+
+warmup();
+
 module.exports = ({ code, license, refactoring, settings }) => {
   if (!verifyLicense(license)) {
     throw 'Invalid license';
