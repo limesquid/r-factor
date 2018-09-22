efb731d4729cfb26f0382a6374c20abca0bb320a
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2018-09-22 02:06:51 +0200

    Remove warmup

--- index.js
+++ index.js
@@ -2,35 +2,7 @@ const globalSettings = require('./src/settings');
 const refactorings = require('./src/refactorings');
 const verifyLicense = require('./src/license/verify');
 
-const WARMUP_CODE = `
-import React, { Component } from 'react';
-
-class WarmUp extends Component {
-  componentDidMount() {
-    this.props.onMount();
-  }
-
-  render() {
-    const { children } = this.props;
-
-    return (
-      <div>{children}</div>
-    );
-  }
-}
-
-export default WarmUp;
-`;
-
-const warmUp = () => {
-  const GeneratePropTypes = refactorings['generate-prop-types'];
-  const generatePropTypes = new GeneratePropTypes();
-  return generatePropTypes.refactor(WARMUP_CODE);
-};
-
-warmUp();
-
-module.exports = ({ code, license, refactoring, settings }) => {
+module.exports = ({ code, license, refactoring, settings = {} }) => {
   if (!verifyLicense(license)) {
     throw 'Invalid license';
   }
